af2214188522df60ea165b72dd3017f2523c1b03
Aayansh Singh <168022023+Ayansh0209@users.noreply.github.com>
2025-07-28 11:10:11 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<vector>
 using namespace std ;
 int main (){
+  int n ;
+  cout << "enter the number";
+  cin >>n;
+  vector<int>numbers(n);
+int sum = 0;
+  for (int i =0 ; i<n; i++){
+    cin >> numbers[i];
+    sum += numbers[i];
+  }
+  double average = static_cast<double>(sum)/n;
+  cout <<"average"<<average<<endl;
 
-cout<<"week";
 return 0;
 }
